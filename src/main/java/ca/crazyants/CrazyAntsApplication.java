17015e80c2ff59430c890249daaee983135c0eaa
sammetav <vishnu.sammeta@tset.com>
2025-07-28 22:14:19 +0200

    first commit

---
+++ src/main/java/ca/crazyants/CrazyAntsApplication.java
@@ -0,0 +1,13 @@
+package ca.crazyants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrazyAntsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrazyAntsApplication.class, args);
+    }
+
+}
